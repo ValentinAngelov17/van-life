8454d6d1c2039a667a4ad686e91d1222691bdc89
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2025-02-12 18:31:03 +0200

    Fixing the nav bar with a Layout route

--- index.jsx
+++ index.jsx
@@ -5,23 +5,20 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from './pages/VansDetail';
+import Layout from './components/Layout';
+
 
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path='/vans' element={<Vans />} />
-        <Route path='/vans/:id' element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path='/vans' element={<Vans />} />
+          <Route path='/vans/:id' element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
