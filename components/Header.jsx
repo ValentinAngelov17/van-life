58016164f634e66c97a3ff6b24c23b6289cee362
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2025-02-17 20:45:24 +0200

    added demo authentication and login page

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,6 +7,12 @@ export default function Header() {
                 <NavLink to="/host" className={({ isActive }) => isActive ? "active-link" : null}>Host</NavLink>
                 <NavLink to="/about" className={({ isActive }) => isActive ? "active-link" : null}>About</NavLink>
                 <NavLink to="/vans" className={({ isActive }) => isActive ? "active-link" : null}>Vans</NavLink>
+                <Link to="login" className="login-link">
+                    <img
+                        src="../assets/images/avatar-icon.png"
+                        className="login-icon"
+                    />
+                </Link>
             </nav>
         </header>
     )
--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,19 +1,35 @@
 import { Link, useSearchParams } from "react-router-dom"
 import "../../server"
 import React from "react"
+import { getVans } from "../../api"
 
 export default function Vans() {
-
-    const [vans, setVans] = React.useState([])
-    React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
-    }, [])
-
     const [searchParams, setSearchParams] = useSearchParams()
+    const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
 
     const typeFilter = searchParams.get("type")
+
+     React.useEffect(() => {
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getVans()
+                setVans(data)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        loadVans()
+    }, [])
+
+    
+
+   
     const possibleTypes = ["simple", "luxury", "rugged"]
     const selectedVans = possibleTypes.includes(typeFilter) ? vans.filter(van => van.type === typeFilter) : vans
 
@@ -52,6 +68,14 @@ export default function Vans() {
             }
             return prevParams
         })
+    }
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
     }
 
     return (
