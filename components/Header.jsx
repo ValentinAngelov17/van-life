ef6331d9bc1aacf99486958ed3870f41a4ab45e4
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2025-02-18 22:31:36 +0200

    small css changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,13 +3,13 @@ export default function Header() {
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
-            <nav>
+            <nav className="base-nav">
                 <NavLink to="/host" className={({ isActive }) => isActive ? "active-link" : null}>Host</NavLink>
                 <NavLink to="/about" className={({ isActive }) => isActive ? "active-link" : null}>About</NavLink>
                 <NavLink to="/vans" className={({ isActive }) => isActive ? "active-link" : null}>Vans</NavLink>
                 <Link to="login" className="login-link">
                     <img
-                        src="../assets/images/avatar-icon.png"
+                        src="assets\images\avatar-icon.png"
                         className="login-icon"
                     />
                 </Link>
