9b8d177943dd88134c211bcb24921968eb9af62b
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2025-02-20 18:56:11 +0200

    modified dashboard and styling

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,3 +1,29 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { BsStarFill } from "react-icons/bs"
+
 export default function Dashboard() {
-    return <h1>Dashboard is here</h1>
+    return (
+        <>
+            <section className="host-dashboard-earnings">
+                <div className="info">
+                    <h1>Welcome!</h1>
+                    <p>Income last <span>30 days</span></p>
+                    <h2>$2,260</h2>
+                </div>
+                <Link to="income">Details</Link>
+            </section>
+            <section className="host-dashboard-reviews">
+                <h2>Review score</h2>
+
+                <BsStarFill className="star" />
+
+                <p>
+                    <span>5.0</span>/5
+                </p>
+                <Link to="reviews">Details</Link>
+            </section>
+           
+        </>
+    )
 }
